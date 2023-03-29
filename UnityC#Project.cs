f3d3353e9415f2c1ebb8adbdc54e55da727006c2
Marcela Torro <99346289+MaahTorro@users.noreply.github.com>
2023-03-29 20:32:23 -0300

    script

---
+++ UnityC#Project.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using unityEngine;
+using unityEngine.UI;
+
+public class LogicProgram : MonoBehaviour
+{
+
+    public Text txtName;
+    public Text txtScore;
+    public Text txdHealth;
+
+    public string playerName;
+    public int point;
+    public float hp;
+
+    void start()
+    {
+
+        txtName.text = playerName;
+        txtScore.text = "Score: " + point;
+        txtHealth.text = "Health: " + hp;
+
+    }
+
+    void update()
+    {
+
+    }
+}
